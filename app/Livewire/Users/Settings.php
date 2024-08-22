2a9e45dd3232b5ff6b329e558d7e2db5997647da
Monteiro Futila <jonquerfutila@gmail.com>
2024-08-22 21:44:27 +0100

    feat: organizar o frontend

---
+++ app/Livewire/Users/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Users;
+
+use Livewire\Component;
+
+class Settings extends Component
+{
+    public function render()
+    {
+        return view('livewire.users.settings');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Home;
+use App\Livewire\LandingPage;
+use App\Livewire\Posts\Create;
+use App\Livewire\Users\Profile;
+use App\Livewire\Users\Settings;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', LandingPage::class);
+Route::get('/home', Home::class)->name('home');
+Route::get('/profile', Profile::class)->name('profile');
+Route::get('/settings', Settings::class)->name('settings');
+Route::get('/posts/create', Create::class)->name('posts.create');
