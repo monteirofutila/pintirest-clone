abd1778bdeaa07769c26c6a57143aa3dbccaa10c
Monteiro Futila <jonquerfutila@gmail.com>
2024-08-26 12:14:39 +0100

    feat: layout card dinamico

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,62 +2,221 @@
     {{-- Care about people's approval and you will be their prisoner. --}}
     <div class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
-                <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
-                    <a href="#">
-                        <img class="w-full h-auto object-cover rounded-3xl"
-                            src="https://i.pinimg.com/564x/86/75/21/867521c3a2985a98418ac44b1cf35243.jpg"
-                            alt="" />
-                    </a>
-                    <div
-                        class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
-                    </div>
-                    <div
-                        class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
-                        <div class="flex justify-end">
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
-                                Guardar
-                            </a>
+            <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-4 space-y-4">
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/originals/43/b9/51/43b9518144a827395adfcf5d407fc465.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
                         </div>
-                        <div class="flex justify-end gap-2">
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
-                                Read
-                            </a>
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
-                                More
-                            </a>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
-                    <a href="#">
-                        <img class="w-full h-auto object-cover rounded-3xl"
-                            src="https://i.pinimg.com/originals/43/b9/51/43b9518144a827395adfcf5d407fc465.jpg"
-                            alt="" />
-                    </a>
-                    <div
-                        class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
-                    </div>
-                    <div
-                        class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
-                        <div class="flex justify-end">
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
-                                Guardar
-                            </a>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/564x/86/75/21/867521c3a2985a98418ac44b1cf35243.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
                         </div>
-                        <div class="flex justify-end gap-2">
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
-                                Read
-                            </a>
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
-                                More
-                            </a>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/originals/4a/3d/ba/4a3dba9ee0369140ae549121dd09b213.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
+                        </div>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/564x/86/75/21/867521c3a2985a98418ac44b1cf35243.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
+                        </div>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/originals/43/b9/51/43b9518144a827395adfcf5d407fc465.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
+                        </div>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/originals/43/b9/51/43b9518144a827395adfcf5d407fc465.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
+                        </div>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative overflow-hidden border border-gray-100 rounded-3xl group">
+                        <a href="#">
+                            <img class="w-full h-auto object-cover rounded-3xl"
+                                src="https://i.pinimg.com/originals/4c/ed/bc/4cedbc454255b40332aa21fb62d85732.jpg"
+                                alt="" />
+                        </a>
+                        <div
+                            class="absolute inset-0 hover:bg-transparent transition duration-300 hover:bg-gray-900 hover:opacity-25 rounded-3xl">
+                        </div>
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between p-5 opacity-0 group-hover:opacity-100 transition duration-300">
+                            <div class="flex justify-end">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-3 font-medium text-center text-white bg-red-600 rounded-full">
+                                    Guardar
+                                </a>
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    Read
+                                </a>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-full">
+                                    More
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -1,3 +1,17 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
+            <div class="columns-1 sm:columns-2 md:columns-2 gap-4 space-y-4">
+
+                <div class="bg-red-100 w-full h-48">
+                    <h2>dddddddddd</h2>
+                </div>
+                <div class="bg-red-100 w-full h-48">
+                    <h2>dddddddddd</h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
