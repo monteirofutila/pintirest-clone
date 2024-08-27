c454413d134e4e1a2eac44cf38324d1b40172b42
Monteiro Futila <jonquerfutila@gmail.com>
2024-08-27 16:54:55 +0100

    feat: formulário

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -1,19 +1,26 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
     <div class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
-            <div class="grid gap-14 md:grid-cols-2 w-full">
-                <div class="flex justify-center w-full">
+        <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto">
+            <div class="w-full flex items-center justify-between border border-gray-300 p-4">
+                <h1 class="text-xl font-medium">Criar Pin</h1>
+                <a href="#"
+                    class="block py-3 px-4 font-medium text-white bg-red-600 hover:bg-red-700 rounded-full">Publicar</a>
+            </div>
+            <div class="w-full max-w-screen-lg flex flex-col sm:flex-row gap-14 mx-auto p-4">
+                <div class="flex flex-[0.5] justify-center">
                     <label for="dropzone-file"
                         class="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-2xl cursor-pointer bg-gray-100 dark:hover:bg-gray-800 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <div class="flex flex-col items-center justify-center pt-5 pb-6 px-4">
                             <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2"
                                     d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                             </svg>
-                            <p class="mb-2 font-medium text-gray-900 dark:text-gray-400">Selecione um ficheiro
+                            <p class="mb-2 font-medium text-gray-900 text-center dark:text-gray-400 w-60">Selecione
+                                um
+                                ficheiro
                                 ou arrasta-o e
                                 larga-o aqui</p>
                         </div>
@@ -21,7 +28,7 @@
                     </label>
                 </div>
 
-                <div class="flex justify-center w-full">
+                <div class="flex flex-1 justify-center">
                     <div class="flex flex-col items-center justify-center w-full space-y-5">
                         <div class="w-full">
                             <label for="title"
